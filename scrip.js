072d5435300744beae3aa025f483eeef3da08f76
Nohuan <64031626+Nohuan@users.noreply.github.com>
2023-11-08 09:04:55 -0600

    finish proyect

--- scrip.js
+++ scrip.js
@@ -1,17 +1,54 @@
-const body = document.querySelector('body')
-let container = document.createElement("div");
-container.className = 'container';
+let color = "black";
+document.addEventListener("DOMContentLoaded", function () {
+  createBoard(16);
+  let btn_popup = document.querySelector("#popup");
+  btn_popup.addEventListener("click", function () {
+    let size = getSize();
+    createBoard(size);
+  });
+});
 
-body.appendChild(container);
-let n = 4;
-for (let i = 0; i < n; i++) {
-  let row = document.createElement("div");
-  row.className = "row";
-  for (let j = 0; j < n; j++) {
-    let colum = document.createElement("div");
-    colum.className = "colum";
-    colum.textContent = "1";
-    row.appendChild(colum);
+function createBoard(size) {
+  let board = document.querySelector(".board");
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let numDivs = size * size;
+
+  for (let i = 0; i < numDivs; i++) {
+    let div = document.createElement("div");
+    div.className = "grid";
+    div.addEventListener("mouseover", colorDiv);
+    board.insertAdjacentElement("beforeend", div);
   }
-  container.appendChild(row);
+}
+
+function getSize() {
+  let input = prompt("What will be the size?");
+  let message = document.querySelector("#message");
+  if (input == "") {
+    message.textContent = "Provide a number";
+  } else if (input < 0 || input > 100) {
+    message.textContent = "Please choose number betweend 1 and 100";
+  } else {
+    message.textContent = "You can play!";
+    return input;
+  }
+}
+
+function colorDiv() {
+  if (color == "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%,50%)`;
+  } else {
+    this.style.backgroundColor = "black";
+  }
+}
+
+function setColor(colorChoice) {
+  color = colorChoice;
+}
+
+function resetBoard() {
+  let divs = document.querySelectorAll(".grid");
+  divs.forEach((div) => (div.style.backgroundColor = "white"));
 }
